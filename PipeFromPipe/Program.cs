3cbcdee8240e4c147aa219837981de3a1d850e24
jinssi <6933196+Jinssi@users.noreply.github.com>
2020-05-11 12:04:29 +0300

    reinit

---
+++ PipeFromPipe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/* A glorius Keanu Reeves app just to show it works.*/
+
+namespace PipeFromPipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You're breathtaking!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# PipeFromPipe
+Name says it all
+
+Create an artifact on the source pipe. Don't touch anything else.
+The source pipe is triggered from a master and we publish an artifact in the end.
+
